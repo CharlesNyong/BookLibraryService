d0d1493084c709f85c43b672aa311e7afabab192
CharlesNyong <asikpo@uwindsor.ca>
2021-04-26 22:47:46 -0400

    modified debug code

--- app/Http/Controllers/updateBook.php
+++ app/Http/Controllers/updateBook.php
@@ -13,9 +13,8 @@ class updateBook extends Controller
     public function index(Request $bookRequest){
         $blnRead = strtolower($bookRequest->read);
 
-        //echo "updating with the following data: ". $status . " date read: ". $bookRequest->dateRead . " Book name: ". $bookRequest->name;
+        //echo "updating with the following date read: ". $bookRequest->dateRead . " Book name: ". $bookRequest->name;
         Book::where("user_id", $bookRequest->userID)->where("BookName", $bookRequest->name)->update(['Read' => $blnRead, 'created_at'=> $bookRequest->dateRead]);
-        
         
         // update successful
         return json_encode(array(1));
