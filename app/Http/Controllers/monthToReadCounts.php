d773c7d5a0a22dc0d651a8a9c234fd71d095dd03
CharlesNyong <asikpo@uwindsor.ca>
2021-04-27 23:38:11 -0400

    modified monthToRead controller to get count of all read books per month

--- app/Http/Controllers/monthToReadCounts.php
+++ app/Http/Controllers/monthToReadCounts.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Book;
 use App\User;
+use DB;
 
 class monthToReadCounts extends Controller
 {
-    //
-
+    
     public function index($userID){
         //$readBooks = array("test");
         // return a query containing the where clause created_at = 'something' and read = '1'
-        $monthToReadCount = Book::where('Read', 1)->where('user_id', $userID)->selectRaw("created_at as Date")->selectRaw("COUNT(created_at) as read_count")->groupBy('created_at')->get();
-        //echo "all books". $readBooks;
+        $monthToReadCount = Book::where('Read', 1)->where('user_id', $userID)->selectRaw("ANY_VALUE(created_at) as Date")->selectRaw("COUNT(books.Read) as read_count")->groupBy(DB::raw('YEAR(created_at)'), DB::raw('MONTH(created_at)'))->get();
+        //echo "mapping: ". $monthToReadCount;
         return json_encode($monthToReadCount); 
     }
 }
