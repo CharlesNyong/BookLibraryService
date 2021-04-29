be81d98b4c7f7a515708d2eeb37be2b6d3869640
CharlesNyong <asikpo@uwindsor.ca>
2021-04-28 23:04:53 -0400

    removed the only_full_group mode from the db config

--- app/Http/Controllers/monthToReadCounts.php
+++ app/Http/Controllers/monthToReadCounts.php
@@ -13,7 +13,7 @@ class monthToReadCounts extends Controller
     public function index($userID){
         //$readBooks = array("test");
         // return a query containing the where clause created_at = 'something' and read = '1'
-        $monthToReadCount = Book::where('Read', 1)->where('user_id', $userID)->selectRaw("ANY_VALUE(created_at) as Date")->selectRaw("COUNT(books.Read) as read_count")->groupBy(DB::raw('YEAR(created_at)'), DB::raw('MONTH(created_at)'))->get();
+        $monthToReadCount = Book::where('Read', 1)->where('user_id', $userID)->selectRaw("created_at as Date")->selectRaw("COUNT(books.Read) as read_count")->groupBy(DB::raw('YEAR(created_at)'), DB::raw('MONTH(created_at)'))->get();
         //echo "mapping: ". $monthToReadCount;
         return json_encode($monthToReadCount); 
     }
